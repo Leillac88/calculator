51768e4696d290d683421a3652ae88cb36b447e9
Leilla Carvalho <148092822+Leillac88@users.noreply.github.com>
2025-05-09 17:00:04 -0300

    add utils.js

---
+++ js/utils.js
@@ -0,0 +1,44 @@
+function formatNumber(number) {
+    const numStr = number.toString()
+    if (numStr === "infinity" || numStr === "-infinity") {
+        return "Erro"
+    }
+
+    if (numStr ==="NaN") {
+        return "Erro"
+    }
+
+    const maxDigits = 12
+
+    if (Math.abs(number) >= 1e12) {
+        return number.toExponencial(6)
+    }
+
+    if (Number.isInteger(Number)) {
+        return numStr.toString()
+    }
+
+    const parts = numStr.split(".")
+
+    if (parts[0].length >= maxDigits) {
+        return number.toExponencial(6)
+    }
+
+    const decimalPlaces = Math.min (maxDigits - parts[0].length - 1, 10)
+    return number.toFixed(decimalPlaces).replace(/\.?0+$/, "")
+}
+
+function isOperator(char) {
+    return ["+", "-", "*", "/"].includes(char)
+}
+
+function getOperatorSymbol(operator) {
+    const symbols = {
+        "+" : "+"
+        "-" : "-"
+        "*" : "x"
+        "/" : "÷"
+    }
+
+    return symbols[operator] || operator
+}
