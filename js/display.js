e125107137f7a4ea0525b7bc8241840db71bf1a5
Leillac88 <leillacarvalho88@gmail.com>
2025-05-09 14:31:41 -0300

    add arquivos

---
+++ js/display.js
@@ -0,0 +1,8 @@
+class display {
+    constructor() {
+        this.displayElement = document.getElementById("display")
+        this.historyElement = document.getElementById("history-display")
+        this.currentValue = 0
+        this.history = ""
+    }
+}
