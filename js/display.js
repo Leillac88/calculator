04c51d3f338ff4bb4a8edc57c3ff9b40f438b597
Leilla Carvalho <148092822+Leillac88@users.noreply.github.com>
2025-05-09 15:06:17 -0300

    add display.js

--- js/display.js
+++ js/display.js
@@ -5,4 +5,41 @@ class display {
         this.currentValue = 0
         this.history = ""
     }
+
+    updateDisplay(value = this.currentValue) {
+        this.currentValue = value
+        this.displayElement.textContent = value
+    }
+
+    updateHistory(value = this.history) {
+        this.currentValue = value
+        this.historyElement.textContent = value
+    }
+
+    clear() {
+        this.updateDisplay("0")
+        this.updateHistory("")
+    }
+
+    appendDigit(digit) {
+        if (this.currentValue === "0" && digit !== ".") {
+            this.updateDisplay(digit)
+        }
+
+        else if (digit === "." && this.currentValue.includes(".")) {
+            return
+        }
+ 
+        else {
+            this.updateDisplay(this.currentValue + digit)       
+        }
+    }
+
+    removeLastDigit() {
+        if (this.currentValue.lenght === 1) {
+            this.updateDisplay("0")
+        } else {
+            this.updateDisplay(this.currentValue.slice(0, -1))
+        }
+    }
 }
