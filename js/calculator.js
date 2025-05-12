5eda4b6d20b93a7c323fa0a681e29a5320f11c4f
Leilla Carvalho <148092822+Leillac88@users.noreply.github.com>
2025-05-12 16:18:05 -0300

    add calculator e others changes

---
+++ js/calculator.js
@@ -0,0 +1,51 @@
+import {display} from './display.js'
+import {getOperatorSymbol} from './utils.js'
+import {formatNumber} from './utils.js'
+
+class Calculator {
+    constructor() {
+        this.display = new Display()
+        this.currentOperand = "0"
+        this.previusOperand = ""
+        this.operation = null
+        this.shouldResetDisplay = false
+        this.lastResult = null
+
+        this.initEventListeners()
+    }
+
+    initEventListeners() {
+        document.querySelectorAll("data-number").forEach((button) => {
+            button.addEventListener("click", () => {
+                this.appendNumber(button.dataset.number)
+            })
+        })
+
+        document.querySelectorAll("data-operation").forEach((button) => {
+            button.addEventListener("click", () => {
+                this.chooseOperation(button.dataset.operation) 
+            })
+        })
+
+        document.querySelectorAll("data-action").forEach((button) => {
+            button.addEventListener("click", () => {
+                const action = button.dataset.action
+
+                switch (action) {
+                    case "clear":
+                        this.clear()
+                        break;
+                    
+                    case "clear":
+                        this.clear()
+                        break;
+                    
+                    case "clear":
+                        this.clear()
+                        break;
+                        
+                }
+            })
+        })
+    }
+}
